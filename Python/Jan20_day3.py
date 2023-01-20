bd2b432b887619cafc29312929ddfe3a33141f18
Akhamesra <akhamesra31@gmail.com>
2023-01-20 12:41:13 +0530

    Abstract class

--- Python/Jan20_day3.py
+++ Python/Jan20_day3.py
@@ -22,6 +22,8 @@ s.set_stud_info("Ragul", 21)
 print(s.get_stud_info())
 print(s.get_sub())
 '''
+#Multiclass Inheritance
+'''
 class Animal:
     def __init__(self,animal):
         self.animal = animal
@@ -49,5 +51,87 @@ class Dog(NonMarineMammal,NonWingedMammal):
 
 d = Dog()
 print(Dog.mro())
+'''
+#Polymorphism
+'''
+class Cat:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def info(self):
+        print(f"I am cat, my name is {self.name} and my age is {self.age}")
+    def speak(self):
+        print("meow!")
 
+class Dog :
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def info(self):
+        print(f"I am dog, my name is {self.name} and my age is {self.age}")
+    def speak(self):
+        print("bark!")
+    
+c1 = Cat("Meow", 5)
+d1 = Dog("Jimmy", 10)
+
+for animal in (c1,d1):
+    animal.info()
+    animal.speak()
+'''
+
+# Method overiding, Abstract function
+'''
+from math import pi
+class Shape:
+    def __init__(self,name):
+        self.name = name
+    def area(self):
+        pass
+    def fact(self):
+        return "2D Shape"
+    def __str__(self):
+        return self.name
+
+class Square(Shape):
+    def __init__(self,length):
+        super().__init__('Square')
+        self.length = length
+    def area(self):
+        return self.length * self.length
+    def fact(self):
+        return "Each Angle is of 90 degree"
+
+class Circle(Shape):
+    def __init__(self,r):
+        super().__init__("Circle")
+        self.r = r
+    def area(self):
+        return pi*self.r*self.r
+    
+s1 = Square(4)
+c1 = Circle(5.6)
+
+print(s1)
+print(s1.area())
+print(s1.fact())
+
+print(c1)
+print(c1.area())
+print(c1.fact())
+'''
+#Asbtract Class
+'''
+from abc import ABC, abstractclassmethod
+class MyClass(ABC):
+    @abstractclassmethod
+    def calc(self,x):
+        pass
+class SubClass(MyClass):
+    def calc(self,x):
+        print("Square = ", x**2)
+
+ob = SubClass()
+ob.calc(15)
+'''
 
