90a7e25fcc78b9cbc02bdfde48ae91eb7ca17d25
Akhamesra <akhamesra31@gmail.com>
2023-01-23 10:13:25 +0530

    Decorators

--- Python/Jan20_day3.py
+++ Python/Jan20_day3.py
@@ -134,7 +134,6 @@ class SubClass(MyClass):
 ob = SubClass()
 ob.calc(15)
 '''
-
 #Exception Handeling 
 '''
 try :
@@ -209,7 +208,7 @@ info 20
 debug 10
 not set 0
 ''' 
-
+'''
 import logging
 logging.basicConfig(filename = "file.log",level=logging.DEBUG)
 logger = logging.getLogger("__name__")
@@ -221,4 +220,75 @@ try:
     logger.info("Done")  
 except ZeroDivisionError as ze:
     logger.exception(ze)
-    
+'''
+
+#Generators
+
+'''def count(n):
+    i = 1
+    while i<n:
+        yield i
+        i+=1
+for i in count(5):
+    print(i)
+'''
+#Iterators
+'''
+class MyIterator:
+    def __init__(self,l,h):
+        self.l=l
+        self.h=h
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.l<=self.h:
+            self.l+=1
+            return self.l-1
+        else:
+            raise StopIteration
+
+it=MyIterator(20,400)
+for i in it:
+    print(i)
+'''
+
+#Decorators
+'''
+def my_decorator(func):
+    def inner(n1,n2):
+        n = 10
+        f = func(n1,n2)
+        return 10+f
+    return inner
+
+@my_decorator
+def func(n1,n2):
+    return n1+n2
+
+# n1 = int(input("Enter a number"))
+# n2 = int(input("Enter second number"))
+# print(func(n1,n2))
+'''
+'''
+def my_decorator(func):
+    def inner(n1,n2):
+        n = 10
+        f = func(n1,n2)
+        return 10+f
+    return inner
+
+def doubleval(myfunc):
+    def inner(a,b):
+        f = myfunc(a,b)
+        return f*2
+    return inner
+
+@doubleval
+@my_decorator   
+def myfunc(a,b):
+    return a**b
+
+print(myfunc(10,2))
+'''
