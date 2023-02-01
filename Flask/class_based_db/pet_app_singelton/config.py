96ea41f00ec2d8115ed27576aac9371881643360
Akhamesra <akhamesra31@gmail.com>
2023-02-01 14:52:14 +0530

    ORM

--- Flask/class_based_db/pet_app_singelton/config.py
+++
@@ -1,5 +0,0 @@
-info = {'host' : 'localhost',
-        'database' : 'Flask_db',
-        'user' : 'postgres',
-        'password' : 'Finserv@2023'}
-        
--- Flask/class_based_db/pet_app_singelton/rough.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-conn = None
-cur = None
-conn = psycopg2.connect(host = 'localhost',
-                    database = 'Flask_db',
-                    user = 'postgres',
-                    password = 'Finserv@2023')
-cur = conn.cursor()
-
